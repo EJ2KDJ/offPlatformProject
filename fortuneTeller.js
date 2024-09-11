4ac062501faeaef84070c168306b53d59edda458
ej2kdj <emmanuelle.salga2007@gmail.com>
2024-09-11 23:22:06 +0800

    fortune message added in array

---
+++ fortuneTeller.js
@@ -0,0 +1,16 @@
+function fortune() {
+    const userFortune = 
+    [
+        "The stars align, signs point to yes.",
+        "Outlook good",
+        "You may rely on it",
+        "Your fate says you must now learn the truth now...",
+        "Your future is vague, try again",
+        "Currently unpredictatble",
+        "Veryyy doubtful",
+        "Unreliable, do not be swayed",
+        "Sources say NO"
+    ];
+
+    
+}
